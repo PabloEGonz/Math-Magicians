4de69005140cdda077fa780a3fc96c4513bcd7ed
Pablo <ezekielgonzalezr@gmail.com>
2023-05-31 21:16:51 -0600

    Create component for result

---
+++ src/components/Result.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Result = ({ total, next, operation }) => {
+  const text = total !== next ? `${total} ${operation}  ${next}` : `${total} ${operation}`;
+  return (
+    <div className="result">
+      <p>
+        {text}
+      </p>
+    </div>
+  );
+};
+
+Result.propTypes = {
+  total: PropTypes.number,
+  next: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  operation: PropTypes.string.isRequired,
+};
+
+Result.defaultProps = {
+  total: 0,
+  next: '',
+};
+// Result.defaultProps = {
+//   result: 0,
+//   next: '',
+//   operation: '',
+// };
+export default Result;
