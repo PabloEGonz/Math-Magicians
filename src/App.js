8831923ac6169a6b363c8c72927e10f3ff9d35e5
Pablo <ezekielgonzalezr@gmail.com>
2023-06-06 12:38:18 -0600

    Add nav bar to app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import './App.css';
+import { Routes, Route, Link } from 'react-router-dom';
+import Home from './components/Home';
 import Calculator from './components/Calculator';
 import Quote from './components/quote';
 
 function App() {
   return (
-    <div className="body">
-      <Calculator />
-      <Quote />
-    </div>
-
+    <>
+      <nav>
+        <h1>Welcome</h1>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/calculator">Calculator</Link></li>
+          <li><Link to="/quote">Quote</Link></li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/calculator" element={<Calculator />} />
+        <Route path="/quote" element={<Quote />} />
+      </Routes>
+    </>
   );
 }
 
