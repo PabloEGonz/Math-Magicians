dba1baa6b4b0d6165c9e14d692311179f920f147
francksefu <90141102+francksefu@users.noreply.github.com>
2023-06-07 22:22:14 +0200

    Add more test for operate.js and operate.js files

--- src/testing/calculate.test.js
+++ src/testing/calculate.test.js
@@ -30,4 +30,30 @@ describe('Testing calculate Function', () => {
         const result = { "next": null, "operation": null, "total": "5" };
         expect(calculate(obj, button)).toStrictEqual(result);
     });
+    test('first condition', () => {
+        const obj = {
+          total: null,
+          next: null,
+          operation: null,
+        };
+        expect(calculate(obj, '1')).toEqual({ next: '1', total: null });
+    });
+    
+    test('second condition', () => {
+        const obj = {
+          total: null,
+          next: '2',
+          operation: null,
+        };
+        expect(calculate(obj, '1')).toEqual({ next: '21', total: null });
+    });
+    
+      test('third condition', () => {
+        const obj = {
+          total: null,
+          next: null,
+          operation: null,
+        };
+        expect(calculate(obj, '=')).toEqual({});
+      });
 })
