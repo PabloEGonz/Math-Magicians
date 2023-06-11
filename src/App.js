03eb2617a3b17107ef596a2b34e1b1a50eb5896e
Pablo <ezekielgonzalezr@gmail.com>
2023-06-10 20:24:50 -0600

    Use router new syntax

--- src/App.js
+++ src/App.js
@@ -1,27 +1,41 @@
 import './App.css';
-import { Routes, Route, Link } from 'react-router-dom';
+import {
+  createBrowserRouter, createRoutesFromElements, RouterProvider, Route, Link, Outlet,
+} from 'react-router-dom';
 import Home from './components/Home';
 import Calculator from './components/Calculator';
 import Quote from './components/quote';
 
 function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/math-magicians" element={<Root />}>
+        <Route index element={<Home />} />
+        <Route path="/math-magicians/calculator" element={<Calculator />} />
+        <Route path="/math-magicians/quote" element={<Quote />} />
+      </Route>,
+    ),
+  );
   return (
     <div className="body">
-      <nav className="flex">
-        <h1 className="nav-brand">Math Magicians</h1>
-        <ul className="flex">
-          <li><Link to="/" className="nav-item">Home</Link></li>
-          <li><Link to="/calculator" className="nav-item">Calculator</Link></li>
-          <li><Link to="/quote" className="nav-item">Quote</Link></li>
-        </ul>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/calculator" element={<Calculator />} />
-        <Route path="/quote" element={<Quote />} />
-      </Routes>
+      <RouterProvider router={router} />
     </div>
   );
 }
+const Root = () => (
+  <>
+    <nav className="flex">
+      <h1 className="nav-brand">Math Magicians</h1>
+      <ul className="flex">
+        <li><Link to="/math-magicians" className="nav-item">Home</Link></li>
+        <li><Link to="/math-magicians/calculator" className="nav-item">Calculator</Link></li>
+        <li><Link to="/math-magicians/quote" className="nav-item">Quote</Link></li>
+      </ul>
+    </nav>
+    <div>
+      <Outlet />
+    </div>
+  </>
+);
 
 export default App;
